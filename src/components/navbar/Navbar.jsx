d0acdb036e3d85c626151ccba2fe74891be82d4b
chanarus <chanarusampath@gmail.com>
2019-03-06 22:32:34 +0530

    initialize navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <span className="navbar-brand">Spotify App</span>
+    </nav>
+  );
+};
+
+export default Navbar;
