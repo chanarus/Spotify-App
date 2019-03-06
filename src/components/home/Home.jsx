1434d83495556d23d3b861facd2ed7839a9cdce6
chanarus <chanarusampath@gmail.com>
2019-03-06 22:32:11 +0530

    initialize home component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Tracks from "../tracks/Tracks";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Tracks />
+    </Fragment>
+  );
+};
+
+export default Home;
