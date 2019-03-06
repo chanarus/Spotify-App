d0a2744be56b8528d1545754f328954909277921
chanarus <chanarusampath@gmail.com>
2019-03-06 22:43:25 +0530

    Added the spotify api call

--- src/context.js
+++ src/context.js
@@ -7,6 +7,22 @@ export class Provider extends Component {
     trackList: [],
     heading: "Top 10 Tracks"
   };
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://api.spotify.com/v1/search?q=slipknot&type=track&market=US&limit=10&offset=5",
+        {
+          headers: {
+            Authorization:
+              "BQD0zxefgyjq0WI5NCXFmelmAujU1ZYAUGjaXNpmaa_XzasnxJt4Sbmhbk99oMFeNN7XQ5BniY32pGyhqzTEY4eJfd_SynoK9aH6ehI6iAw0vi8qnqUpCjLS9hvpCdHZDvJUbi9Zt5uq-yZRhNJYaXIvhzSB_ZCyA-54uYCr0zUoajGFz0VZ"
+          }
+        }
+      )
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return <Context value={this.state}>{this.props.children}</Context>;
   }
