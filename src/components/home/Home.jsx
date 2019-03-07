6e85d56a59b16050ebb2a470bdb3b6b58f516b36
chanaru <rajapaksha.chanarusampath@netstarter.com>
2019-03-07 11:03:51 +0530

    Added the search component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import Tracks from "../tracks/Tracks";
+import Search from "../search/Search";
 
 const Home = () => {
   return (
     <Fragment>
+      <Search />
       <Tracks />
     </Fragment>
   );
