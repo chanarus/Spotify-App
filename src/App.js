06417b1fc916a13c6f563574d792048e02d86aef
The Game <chanarusampath@gmail.com>
2019-03-10 09:06:29 +0530

    Removed the unwanted scripts and dependancies

--- src/App.js
+++ src/App.js
@@ -2,31 +2,24 @@ import React, { Component, Fragment } from "react";
 import { Provider, Consumer } from "./context";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
-import Login from "./components/login/Login";
 
 class App extends Component {
-  render() {
-    return (
-      <Provider>
-        <Fragment>
-          <Navbar />
-          <Consumer>
-            {value => {
-              if (value.loggedIn) {
-                return (
-                  <div className="container">
-                    <Home />
-                  </div>
-                );
-              } else {
-                return <Login />;
-              }
-            }}
-          </Consumer>
-        </Fragment>
-      </Provider>
-    );
-  }
+	render() {
+		return (
+			<Provider>
+				<Fragment>
+					<Navbar />
+					<Consumer>
+						{value => (
+							<div className="container">
+								<Home />
+							</div>
+						)}
+					</Consumer>
+				</Fragment>
+			</Provider>
+		);
+	}
 }
 
 export default App;
--- src/context.js
+++ src/context.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import request from "request";
+import { client_id, client_secret } from "./config/key";
 
 const Context = React.createContext();
 
@@ -10,77 +12,81 @@ const Context = React.createContext();
  * @param action
  */
 const reducer = (state, action) => {
-  switch (action.type) {
-    case "SEARCH":
-      return {
-        ...state,
-        trackList: action.payload,
-        heading: action.heading,
-        searchType: action.searchType,
-        noResult: action.noResult
-      };
-    default:
-      return state;
-  }
-};
-
-/**
- * Obtains parameters from the hash of the URL
- * @return Object
- */
-export const getHashParams = () => {
-  let hashParams = {};
-  let e,
-    r = /([^&;=]+)=?([^&;]*)/g,
-    q = window.location.hash.substring(1);
-  while ((e = r.exec(q))) {
-    hashParams[e[1]] = decodeURIComponent(e[2]);
-  }
-  return hashParams;
+	switch (action.type) {
+		case "SEARCH":
+			return {
+				...state,
+				trackList: action.payload,
+				heading: action.heading,
+				searchType: action.searchType,
+				noResult: action.noResult
+			};
+		default:
+			return state;
+	}
 };
 
 export class Provider extends Component {
-  constructor() {
-    super();
-    const params = getHashParams();
-    this.state = {
-      trackList: [],
-      heading: "Top 10 Tracks",
-      searchType: "track",
-      noResult: false,
-      loggedIn: params.access_token ? true : false,
-      dispatch: action => this.setState(state => reducer(state, action))
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			trackList: [],
+			heading: "Top 10 Tracks",
+			searchType: "track",
+			noResult: false,
+			dispatch: action => this.setState(state => reducer(state, action))
+		};
+	}
 
-  /**
-   * Initial request for the api to load data.
-   */
-  componentDidMount() {
-    axios
-      .get(
-        "https://api.spotify.com/v1/search?q=linkin%20park&type=track&market=US&limit=20&offset=0",
-        {
-          headers: {
-            Authorization: `Bearer ${getHashParams().access_token}`
-          }
-        }
-      )
-      .then(res => {
-        this.setState({
-          trackList: res.data.tracks.items
-        });
-      })
-      .catch(err => console.log(err));
-  }
+	/**
+	 * Initial request for the api to load data.
+	 */
+	componentDidMount() {
+		const authOptions = {
+			url:
+				"https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token",
+			headers: {
+				Authorization:
+					"Basic " +
+					new Buffer(client_id + ":" + client_secret).toString(
+						"base64"
+					)
+			},
+			form: {
+				grant_type: "client_credentials"
+			},
+			json: true
+		};
+		request.post(authOptions, (error, response, body) => {
+			if (!error && response.statusCode === 200) {
+				// use the access token to access the Spotify Web API
+				const token = body.access_token;
+				axios
+					.get(
+						"https://api.spotify.com/v1/search?q=linkin%20park&type=track&market=US&limit=20&offset=0",
+						{
+							headers: {
+								Authorization: `Bearer ${token}`
+							}
+						}
+					)
+					.then(res => {
+						this.setState({
+							trackList: res.data.tracks.items
+						});
+					})
+					.catch(err => console.log(err));
+			}
+		});
+	}
 
-  render() {
-    return (
-      <Context.Provider value={this.state}>
-        {this.props.children}
-      </Context.Provider>
-    );
-  }
+	render() {
+		return (
+			<Context.Provider value={this.state}>
+				{this.props.children}
+			</Context.Provider>
+		);
+	}
 }
 
 export const Consumer = Context.Consumer;
